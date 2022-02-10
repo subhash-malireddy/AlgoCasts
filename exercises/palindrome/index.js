bbca53639566923f3e174dfbeb28ad6c0bafaf11
subhash_malireddy <ssm934@uowmail.edu.au>
2022-02-11 10:02:17 +1100

    my solution for palindrome challenge

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,11 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    const reversed = str.split("").reverse().join("")
+    return reversed === str ? true : false
+}
+
+
 
 module.exports = palindrome;

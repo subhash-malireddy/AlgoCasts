f6673073336dc77dab8f16779a3144e3d8f69595
subhash_malireddy <ssm934@uowmail.edu.au>
2022-02-10 21:12:34 +1100

    my solution fo reverse of a string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,17 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    var strArr = str.split("")
+    
+    var len = strArr.length;
+    
+    for(var i=0; i<len/2; i++){
+        let temp = strArr[i];
+        strArr[i] = strArr[len - i];
+        strArr[len - i] = temp;
+    }
+    return strArr.join("")
+}
 
 module.exports = reverse;

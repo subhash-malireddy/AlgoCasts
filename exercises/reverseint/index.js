78f6bb18ef4072d49f24ee3ef3faa02bfa572139
subhash_malireddy <ssm934@uowmail.edu.au>
2022-02-11 12:26:01 +1100

    implementing classic algo for reversing an int in JavaScript

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,17 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    // return parseInt(Math.abs(n).toString().split("").reverse().join("")) * Math.sign(n)
+    let reverse = 0, lastDigit = 0
+    const sign = Math.sign(n) 
+    n = Math.abs(n)
+    while(n > 0){
+        lastDigit = n % 10
+        reverse = reverse * 10 + lastDigit
+        n = Math.floor(n /10)
+    }
+    return reverse * sign
+}
 
 module.exports = reverseInt;

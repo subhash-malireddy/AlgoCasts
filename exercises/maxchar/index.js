47040d304c92f316ae327615e209618400cf3d47
subhash_malireddy <ssm934@uowmail.edu.au>
2022-02-11 15:23:29 +1100

    charMap technique for maxChar.

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,12 +5,15 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {
-    let charSet = new Set([])
-    let charCount = {}
+/*
+    my original solution:
+    
+    function maxChar(str) {
     if(str.length === 1){
         return str
     }
+    let charSet = new Set([])
+    let charCount = {}
     for(const char of str){
         if(charSet.has(char)){
             charCount[char] = charCount[char] + 1 || 2
@@ -20,5 +23,36 @@ function maxChar(str) {
     }
     return Object.keys(charCount)[0] || "👯‍♂️ All characters occured the same number of times in the given string! 👯‍♀️"
 }
-console.log(maxChar("ab"))
+*/
+
+// adapted to tutorial and much robust. It creates a charMap
+function maxChar(str) {
+    if (str.length === 1) {
+        return str
+    }
+    let charCount = {}
+    for (const char of str) {
+        if (charCount.hasOwnProperty(char)) {
+            charCount[char] = charCount[char] + 1
+        } else {
+            charCount[char] = 1
+        }
+        /*
+            Above if else block OR simply
+            charCount[char] = charCount[char] + 1 || 1
+        */
+    }
+
+    let maxChar = "", maxCharCount = 0
+
+    for (const [char, count] of Object.entries(charCount)) {
+        if (count > maxCharCount) {
+            maxCharCount = count
+            maxChar = char
+        }
+    }
+
+    return maxChar
+}
+console.log(maxChar("abcccccccd"))
 module.exports = maxChar;

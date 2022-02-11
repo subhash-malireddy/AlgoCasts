7614e5b3b613051df731f7d562a602cb0a2aa007
subhash_malireddy <ssm934@uowmail.edu.au>
2022-02-11 13:13:38 +1100

    My solution for maxChar challenge.

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
-
+function maxChar(str) {
+    let charSet = new Set([])
+    let charCount = {}
+    if(str.length === 1){
+        return str
+    }
+    for(const char of str){
+        if(charSet.has(char)){
+            charCount[char] = charCount[char] ? charCount[char] + 1 : 2
+        }else{
+            charSet.add(char)
+        }
+    }
+    return Object.keys(charCount)[0] || "👯‍♂️ All characters occured the same number of times in the given string! 👯‍♀️"
+}
+console.log(maxChar("ab"))
 module.exports = maxChar;

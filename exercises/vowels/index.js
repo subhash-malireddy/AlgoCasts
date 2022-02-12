23fb78062aef7f895b1fe68157c72fe128583be0
subhash_malireddy <ssm934@uowmail.edu.au>
2022-02-12 16:45:23 +1100

    my solution for vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,9 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    const vowels = str.match(/a|e|i|o|u/ig)
+    return vowels ? vowels.length : 0
+}
 
 module.exports = vowels;

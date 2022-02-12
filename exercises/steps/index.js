d8bf7a7e17a551bdfa3a9874cb6709f2bd482ba6
subhash_malireddy <ssm934@uowmail.edu.au>
2022-02-12 21:53:07 +1100

    My solution for steps when drunk 😵

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,37 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    let i = 1
+    if (Math.sign(n) === -1) console.log('Provide a postive integer for n.')
+    if (n === 0) console.log('')
+    if (n === 1) {
+        console.log(`#`)
+        return
+    }
+    while (i <= n) {
+        let steps = ""
+
+        // # loop
+        let hindex = 1
+        while (hindex <= i) {
+            steps += "#"
+            hindex++
+        }
+
+        //" " loop
+        let maxNumSpaces = n - i, spaceIndex = 1
+        while (spaceIndex <= maxNumSpaces) {
+            steps += " "
+            spaceIndex++
+        }
+
+        console.log(steps)
+
+        i++
+    }
+}
+
+steps(1)
 
 module.exports = steps;

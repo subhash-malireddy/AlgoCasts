e4ad5d8e1ef530351636095705a9e5a5da7d3118
subhash_malireddy <ssm934@uowmail.edu.au>
2022-02-12 15:36:46 +1100

    my solution for anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,12 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    const charA = stringA.toLowerCase().match(/\w/g).sort()
+    const charB = stringB.toLowerCase().match(/\w/g).sort()
 
+    return charA.every((char, index) => char === charB[index])
+}
+
+console.log(anagrams('RAIL! SAFETY!', 'fairy tales'))
 module.exports = anagrams;

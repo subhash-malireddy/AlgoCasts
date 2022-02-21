43730c069e0c1116b740840474ea4c724533e321
subhash_malireddy <ssm934@uowmail.edu.au>
2022-02-22 10:53:03 +1100

    my recursion for steps problem

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,37 +17,56 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-    let i = 1
-    if (Math.sign(n) === -1) console.log('Provide a postive integer for n.')
-    if (n === 0) console.log('')
-    if (n === 1) {
-        console.log(`#`)
+// function steps(n) {
+//     let i = 1
+//     if (Math.sign(n) === -1) console.log('Provide a postive integer for n.')
+//     if (n === 0) console.log('')
+//     if (n === 1) {
+//         console.log(`#`)
+//         return
+//     }
+//     while (i <= n) {
+//         let steps = ""
+
+//         // # loop
+//         let hindex = 1
+//         while (hindex <= i) {
+//             steps += "#"
+//             hindex++
+//         }
+
+//         //" " loop
+//         let maxNumSpaces = n - i, spaceIndex = 1
+//         while (spaceIndex <= maxNumSpaces) {
+//             steps += " "
+//             spaceIndex++
+//         }
+
+//         console.log(steps)
+
+//         i++
+//     }
+// }
+/**
+*@param {number} n 
+*@param {number} stNum
+**/
+function steps(n, stNum = 1) {
+    if (stNum > n) {
         return
     }
-    while (i <= n) {
-        let steps = ""
-
-        // # loop
-        let hindex = 1
-        while (hindex <= i) {
-            steps += "#"
-            hindex++
-        }
-
-        //" " loop
-        let maxNumSpaces = n - i, spaceIndex = 1
-        while (spaceIndex <= maxNumSpaces) {
-            steps += " "
-            spaceIndex++
-        }
-
-        console.log(steps)
-
-        i++
+    let stair = ""
+    while (stair.length < stNum) {
+        stair += '#'
     }
+    // stair.padEnd()
+    while (stair.length < n) {
+        stair += ' '
+    }
+    console.log(stair)
+    steps(n, stNum + 1)
 }
 
-steps(1)
+steps(5)
 
 module.exports = steps;

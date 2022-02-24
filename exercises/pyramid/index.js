feb3957406c0ff7d533f7c3dec95623eae44ea16
subhash_malireddy <ssm934@uowmail.edu.au>
2022-02-24 12:08:17 +1100

    my iterative approach for pyramid probelm

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -9,11 +9,28 @@
 //   pyramid(2)
 //       ' # '
 //       '###'
-//   pyramid(3)
-//       '  #  '
-//       ' ### '
-//       '#####'
+//   pyramid(5)
+//       '    #    '
+//       '   ###   '
+//       '  #####  '
+//       ' ####### '
+//       '#########'
+function pyramid(n) {
+    const stairLength = 1 + 2 * (n - 1)
+    // const centerIndex = n // Math.ceil(stairLength / 2) gives n  
+    // console.log("stairLength : ", stairLength, '\n', "centerIndex : ", n)
+    let numHashes = 1
+    for (let i = 0; i < n; i++) {
+        let stair = ""
+        while (stair.length < numHashes) {
+            stair += "#"
+        }
+        stair = stair.padStart(stair.length + (stairLength - numHashes) / 2)
+        stair = stair.padEnd(stairLength)
+        console.log(stair)
+        numHashes += 2
+    }
+}
 
-function pyramid(n) {}
-
+// pyramid(3)
 module.exports = pyramid;

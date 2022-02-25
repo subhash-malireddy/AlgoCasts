58d8fcd70da07bf7f99013f4b9916116c44483b4
subhash_malireddy <ssm934@uowmail.edu.au>
2022-02-25 17:07:22 +1100

    recussive solution for pyramid problem

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -15,22 +15,46 @@
 //       '  #####  '
 //       ' ####### '
 //       '#########'
-function pyramid(n) {
-    const stairLength = 1 + 2 * (n - 1)
-    // const centerIndex = n // Math.ceil(stairLength / 2) gives n  
-    // console.log("stairLength : ", stairLength, '\n', "centerIndex : ", n)
-    let numHashes = 1
-    for (let i = 0; i < n; i++) {
-        let stair = ""
-        while (stair.length < numHashes) {
-            stair += "#"
-        }
-        stair = stair.padStart(stair.length + (stairLength - numHashes) / 2)
-        stair = stair.padEnd(stairLength)
-        console.log(stair)
-        numHashes += 2
+
+// using loops
+// function pyramid(n) {
+//     const stairLength = 1 + 2 * (n - 1)
+//     // const centerIndex = n // Math.ceil(stairLength / 2) gives n  
+//     // console.log("stairLength : ", stairLength, '\n', "centerIndex : ", n)
+//     let numHashes = 1
+//     for (let i = 0; i < n; i++) {
+//         let stair = ""
+//         while (stair.length < numHashes) {
+//             stair += "#"
+//         }
+//         stair = stair.padStart(stair.length + (stairLength - numHashes) / 2)
+//         stair = stair.padEnd(stairLength)
+//         console.log(stair)
+//         numHashes += 2
+//     }
+// }
+/**
+ * 
+ * @param {number} n 
+ * @param {number} levelLength 
+ * @param {number} stair 
+ * @param {number} numHashes
+ */
+
+
+function pyramid(n, stairNum = 0, numHashes = 1, stairLength = 2 * n - 1) {
+    if (stairNum === n) return
+    let stair = ""
+    while (stair.length < numHashes) {
+        stair += '#'
     }
+    stair = stair.padStart(stair.length + (stairLength - numHashes) / 2)
+    stair = stair.padEnd(stairLength)
+    console.log(stair)
+    numHashes += 2
+    stairNum++
+    pyramid(n, stairNum, numHashes)
 }
 
-// pyramid(3)
+pyramid(3)
 module.exports = pyramid;
